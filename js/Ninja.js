fe4e2bbe80536e9939564fab29a68fe95e473a15
Thomas <126Twaslh@frhsd.com>
2024-10-08 14:04:36 -0400

    first commit

---
+++ js/Ninja.js
@@ -0,0 +1,17 @@
+//Ep 1
+console.log(1);
+console.log(2);
+// Ep 2
+let age = 25;
+let year = 2024
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+var score = 75;
+console.log(score);
