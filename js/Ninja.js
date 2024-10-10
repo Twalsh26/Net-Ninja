3c420fe5bd29e022c162d0bb1ec527a13d419591
Thomas <126Twaslh@frhsd.com>
2024-10-10 12:48:35 -0400

    ARRAYS

--- js/Ninja.js
+++ js/Ninja.js
@@ -4,6 +4,7 @@ console.log(2);
 // Ep 2
 let age = 25;
 let year = 2024
+// log things to console
 console.log(age, year);
 
 age = 30;
@@ -15,3 +16,49 @@ console.log(points);
 
 var score = 75;
 console.log(score);
+
+//string
+console.log('Hello world');
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+//stirng concatenation
+let firstname = 'Brandonlet' ;
+let lastName = 'Sanderson' ;
+
+let fullName = firstName  + ' ' + lastName;
+console.log(fullName);
+//Getting characters
+console.log(fullname[2]);
+//string length
+console.log(fullName.length)
+//string methods
+console.log(fullName.toUpperCase());
+let result = FullName.toLowercase();
+console.log(result, fullName);
+
+//Ep 3
+//comon string methods
+let emails = 'mario@thenetninja.co.uk'
+// Let result = email.lastIndexOf ('n');
+
+// Let result = email.slice (2,5);
+
+// Let result = email.substr (4,10);
+
+// Let result = email.replace ('m' , 'w');
+let results = emails.replace('n' , 'w')
+console.log(result);
+
+//Ep 4
+let radius = 10;
+const pit = 3.15;
+//console.lg(radius, pi);
+
+// math oprators +, -, *, /, **, %
+
+//console.log(10 / 2);
+//let result = radius % 3
+//let result = pi * radius**2
+// order of operation - B I D M A S
+
+console.log(result);
